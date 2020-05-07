50ce871ceff93193b6a359c226a822ce776f99b4
Stephan Jahrling <stephan@coding-rockstar.de>
2020-05-07 13:50:27 +0200

    add new version of cnn for training / prediction

---
+++ Tf-Text-Classification-legalbranch/predict_cnn_v2.py
@@ -0,0 +1,143 @@
+# train a model for text classification with tensorflow / keras
+from __future__ import absolute_import, division, print_function
+
+import re
+import json
+import pathlib
+
+import string
+import numpy as np
+import pandas as pd
+
+
+# nltk
+import nltk
+# nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+
+
+# Keras
+import tensorflow as tf
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras import utils
+from sklearn.preprocessing import LabelBinarizer, LabelEncoder
+
+
+
+def load_data(filename):
+    data = []
+    with open(str(pathlib.Path(__file__).parent.absolute()) + "/" + filename, 'r') as f:
+        imported = json.load(f)
+        data = pd.DataFrame(imported['data'])
+        data.drop(['id', 'caseTitle', 'lbName', 'ltName', 'ltAlias'], axis=1, inplace=True)
+
+    return data
+
+### Text Normalizing function. Part of the following function was taken from this link. 
+### https://medium.com/@sabber/classifying-yelp-review-comments-using-lstm-and-word-embeddings-part-1-eb2275e4066b
+def clean_text(text):
+    """
+        text: a string
+        
+        return: modified initial string
+    """
+    ## Remove puncuation
+    text = text.translate(string.punctuation)
+    
+    ## Convert words to lower case and split them
+    text = text.lower().split()
+    
+    ## Remove stop words
+    stops = set(stopwords.words("german"))
+    text = [w for w in text if not w in stops and len(w) >= 3]
+    
+    text = " ".join(text)
+    ## Clean the text
+    text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=äöüß]", " ", text)
+    text = re.sub(r"\'s", " ", text)
+    text = re.sub(r",", " ", text)
+    text = re.sub(r"\.", " ", text)
+    text = re.sub(r"!", " ! ", text)
+    text = re.sub(r"\/", " ", text)
+    text = re.sub(r"\^", " ^ ", text)
+    text = re.sub(r"\+", " + ", text)
+    text = re.sub(r"\-", " - ", text)
+    text = re.sub(r"\=", " = ", text)
+    text = re.sub(r"'", " ", text)
+    text = re.sub(r"(\d+)(k)", r"\g<1>000", text)
+    text = re.sub(r":", " : ", text)
+    text = re.sub(r" e g ", " eg ", text)
+    text = re.sub(r" b g ", " bg ", text)
+    text = re.sub(r" u s ", " american ", text)
+    text = re.sub(r"\0s", "0", text)
+    text = re.sub(r" 9 11 ", "911", text)
+    text = re.sub(r"e-mail", "email", text)
+    text = re.sub(r"j k", "jk", text)
+    text = re.sub(r"\s{2,}", " ", text)
+    
+    ## Stemming
+    text = text.split()
+    stemmer = SnowballStemmer('german')
+    stemmed_words = [stemmer.stem(word) for word in text]
+    text = " ".join(stemmed_words)
+
+    return text
+
+
+# load data and remove null-Values
+df = load_data("data.json")
+top10lb = list(df['lbAlias'].value_counts().head(10).index)
+df = df[df['lbAlias'].isin(top10lb)]
+
+df = df.dropna()
+df = df[df.caseDesc.apply(lambda x: x != "")]
+df = df[df.lbAlias.apply(lambda x: x != "")]
+
+# apply text cleaning function to df['text']
+df['caseDesc'] = df['caseDesc'].map(lambda x: clean_text(x))
+
+
+train_size = int(len(df) * .8)
+train_posts = df['caseDesc'][:train_size]
+train_tags = df['lbAlias'][:train_size]
+
+test_posts = df['caseDesc'][train_size:]
+test_tags = df['lbAlias'][train_size:]
+
+max_words = 25000
+tokenize = Tokenizer(num_words=max_words, char_level=False)
+tokenize.fit_on_texts(train_posts) # only fit on train
+
+x_train = tokenize.texts_to_matrix(train_posts)
+
+encoder = LabelEncoder()
+encoder.fit(train_tags)
+y_train = encoder.transform(train_tags)
+labels = encoder.classes_
+
+num_classes = np.max(y_train) + 1
+y_train = utils.to_categorical(y_train, num_classes)
+
+
+# load / initiate model
+model = tf.keras.models.load_model(str(pathlib.Path(__file__).parent.absolute()) + "/legalcase_cnn_model.h5")
+
+
+
+# make preditction on this model
+validation_data = load_data("legalcase_validation.json")
+
+# filter by known legal branches
+validation_data = validation_data[validation_data['lbAlias'].isin(top10lb)]
+
+# sanitize text
+validation_data['caseDesc'] = validation_data['caseDesc'].map(lambda x: clean_text(x))
+
+for index, row in validation_data.iterrows():
+    seq = tokenize.texts_to_matrix([row['caseDesc']])
+    predictions = model.predict(seq)
+    pred = predictions[0]
+    print("Legal branch predicted: {} (Soll: {}) with {}".format(labels[np.argmax(pred)], row['lbAlias'], pred[np.argmax(pred)]))
+    
---
+++ Tf-Text-Classification-legalbranch/train_cnn_v2.py
@@ -0,0 +1,186 @@
+# train a model for text classification with tensorflow / keras
+from __future__ import absolute_import, division, print_function
+
+import re
+import json
+import pathlib
+
+import string
+import numpy as np
+import pandas as pd
+
+
+# nltk
+import nltk
+# nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+
+
+# Keras / sklearn
+import tensorflow as tf
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras import utils
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer, LabelEncoder
+
+
+
+# plot
+import matplotlib.pyplot as plt
+
+
+
+def load_data():
+    data = []
+    with open(str(pathlib.Path(__file__).parent.absolute()) + "/data.json", 'r') as f:
+        imported = json.load(f)
+        data = pd.DataFrame(imported['data'])
+        data.drop(['id', 'caseTitle', 'lbName', 'ltName', 'ltAlias'], axis=1, inplace=True)
+
+    return data
+
+### Text Normalizing function. Part of the following function was taken from this link. 
+### https://medium.com/@sabber/classifying-yelp-review-comments-using-lstm-and-word-embeddings-part-1-eb2275e4066b
+def clean_text(text):
+    """
+        text: a string
+        
+        return: modified initial string
+    """
+    ## Remove puncuation
+    text = text.translate(string.punctuation)
+    
+    ## Convert words to lower case and split them
+    text = text.lower().split()
+    
+    ## Remove stop words
+    stops = set(stopwords.words("german"))
+    text = [w for w in text if not w in stops and len(w) >= 3]
+    
+    text = " ".join(text)
+    ## Clean the text
+    text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=äöüß]", " ", text)
+    text = re.sub(r"\'s", " ", text)
+    text = re.sub(r",", " ", text)
+    text = re.sub(r"\.", " ", text)
+    text = re.sub(r"!", " ! ", text)
+    text = re.sub(r"\/", " ", text)
+    text = re.sub(r"\^", " ^ ", text)
+    text = re.sub(r"\+", " + ", text)
+    text = re.sub(r"\-", " - ", text)
+    text = re.sub(r"\=", " = ", text)
+    text = re.sub(r"'", " ", text)
+    text = re.sub(r"(\d+)(k)", r"\g<1>000", text)
+    text = re.sub(r":", " : ", text)
+    text = re.sub(r" e g ", " eg ", text)
+    text = re.sub(r" b g ", " bg ", text)
+    text = re.sub(r" u s ", " american ", text)
+    text = re.sub(r"\0s", "0", text)
+    text = re.sub(r" 9 11 ", "911", text)
+    text = re.sub(r"e-mail", "email", text)
+    text = re.sub(r"j k", "jk", text)
+    text = re.sub(r"\s{2,}", " ", text)
+    
+    ## Stemming
+    text = text.split()
+    stemmer = SnowballStemmer('german')
+    stemmed_words = [stemmer.stem(word) for word in text]
+    text = " ".join(stemmed_words)
+
+    return text
+
+
+
+# load data and remove null-Values
+df = load_data()
+top10lb = list(df['lbAlias'].value_counts().head(10).index)
+df = df[df['lbAlias'].isin(top10lb)]
+
+df = df.dropna()
+df = df[df.caseDesc.apply(lambda x: x != "")]
+df = df[df.lbAlias.apply(lambda x: x != "")]
+
+# apply text cleaning function to df['text']
+df['caseDesc'] = df['caseDesc'].map(lambda x: clean_text(x))
+
+# print(df['lbAlias'].value_counts())
+# print(df.head(10))
+
+
+train_size = int(len(df) * .8)
+train_posts = df['caseDesc'][:train_size]
+train_tags = df['lbAlias'][:train_size]
+
+test_posts = df['caseDesc'][train_size:]
+test_tags = df['lbAlias'][train_size:]
+
+max_words = 25000
+tokenize = Tokenizer(num_words=max_words, char_level=False)
+tokenize.fit_on_texts(train_posts) # only fit on train
+
+x_train = tokenize.texts_to_matrix(train_posts)
+x_test = tokenize.texts_to_matrix(test_posts)
+
+encoder = LabelEncoder()
+encoder.fit(train_tags)
+y_train = encoder.transform(train_tags)
+y_test = encoder.transform(test_tags)
+
+num_classes = np.max(y_train) + 1
+y_train = utils.to_categorical(y_train, num_classes)
+y_test = utils.to_categorical(y_test, num_classes)
+
+
+# create model
+def create_model():
+    """
+        create model
+        https://towardsdatascience.com/multi-class-text-classification-model-comparison-and-selection-5eb066197568
+    """
+    model = Sequential()
+    model.add(Dense(512, input_shape=(max_words,)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    batch_size = 64
+    epochs = 2
+
+    history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_split=0.1)
+
+    return model, history
+
+
+model, history = create_model()
+
+
+# evaluate model
+accr = model.evaluate(x_test, y_test)
+print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0], accr[1]))
+
+
+# create graphics
+def plot_graphs(history, string):
+    plt.plot(history.history[string])
+    plt.plot(history.history['val_'+string])
+    plt.xlabel("Epochs")
+    plt.ylabel(string)
+    plt.legend([string, 'val_'+string])
+    plt.show()
+  
+# plot_graphs(history, "accuracy")
+# plot_graphs(history, "loss")
+
+model.save(str(pathlib.Path(__file__).parent.absolute()) + "/legalcase_cnn_model.h5")
+print("Saved model to disk")
